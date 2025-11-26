0f4473fa8f713de38d251b9787b2e92ae60a06aa
apple <apple@apples-MacBook-Pro.local>
2025-11-26 14:59:05 +0530

    Calculator code

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-const timer = document.querySelector('#time span')
+function add() {
+  let a = +document.getElementById('num1').value;
+  let b = +document.getElementById('num2').value;
+  document.getElementById('result').innerText = a + b;
+}
 
-setInterval(() => {
-	timer.innerText = new Date().toLocaleString()
-}, 1000)
+function subtract() {
+  let a = +document.getElementById('num1').value;
+  let b = +document.getElementById('num2').value;
+  document.getElementById('result').innerText = a - b;
+}
+
+function multiply() {
+  let a = +document.getElementById('num1').value;
+  let b = +document.getElementById('num2').value;
+  document.getElementById('result').innerText = a * b;
+}
+
+function divide() {
+  let a = +document.getElementById('num1').value;
+  let b = +document.getElementById('num2').value;
+  if (b === 0) {
+    document.getElementById('result').innerText = "Cannot divide by zero";
+    return;
+  }
+  document.getElementById('result').innerText = a / b;
+}
