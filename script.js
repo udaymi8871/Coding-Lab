fa981b050bd7e68873779b75ebf396cdf4a43576
apple <apple@apples-MacBook-Pro.local>
2025-11-26 14:52:44 +0530

    html/css starting point

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-function add() {
-    let a = +document.getElementById('num1').value;
-    let b = +document.getElementById('num2').value;
-    document.getElementById('result').innerText = a + b;
-  }
-  
-  function subtract() {
-    let a = +document.getElementById('num1').value;
-    let b = +document.getElementById('num2').value;
-    document.getElementById('result').innerText = a - b;
-  }
-  
-  function multiply() {
-    let a = +document.getElementById('num1').value;
-    let b = +document.getElementById('num2').value;
-    document.getElementById('result').innerText = a * b;
-  }
-  
-  function divide() {
-    let a = +document.getElementById('num1').value;
-    let b = +document.getElementById('num2').value;
-    if (b === 0) {
-      document.getElementById('result').innerText = "Cannot divide by zero";
-      return;
-    }
-    document.getElementById('result').innerText = a / b;
-  }
-  
+const timer = document.querySelector('#time span')
+
+setInterval(() => {
+	timer.innerText = new Date().toLocaleString()
+}, 1000)
